b4329d0456f39d3560bd73957f86ca8983a8db1b
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-10 01:44:25 +0430

    Create Func.php

---
+++ Func.php
@@ -0,0 +1,11 @@
+<?php
+function is_number($qhbdthvcd9){
+if(preg_match("/^(-){0,1}([0-9]+)(,[0-9][0-9][0-9])*([.][0-9]){0,1}([0-9]*)$/",$qhbdthvcd9) == 1){
+  
+  echo true;
+}
+else{
+}
+  echo false;
+}
+?>
