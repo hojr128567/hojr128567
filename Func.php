725c22989ade0cb1ae40f4e1f45717e8656e8f72
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-10 02:25:45 +0430

    Update Func.php

--- Func.php
+++ Func.php
@@ -1,11 +1,1 @@
-<?php
-function is_number($qhbdthvcd9){
-if(preg_match("/^(-){0,1}([0-9]+)(,[0-9][0-9][0-9])*([.][0-9]){0,1}([0-9]*)$/",$qhbdthvcd9) == 1){
-  
-  echo true;
-}
-else{
-}
-  echo false;
-}
-?>
+
