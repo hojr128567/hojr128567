0ba1eda786856c4f0e13c77e26131a6b3b4f96f2
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-10 04:05:01 +0430

    Create Func.php

--- Func.php
+++ Func.php
@@ -1,1 +1,25 @@
-
+<?php
+function Redirect($ayhvsr128){
+  compile("<script>window.location = $ayhvsr;</script>");
+  
+}
+function p($frfefe){
+  compile("<p>$frfefe</p>");
+  
+}
+function set_icon($ayhdrufe){
+  compile("<link rel='shortcut icon' type='image/x-icon' href=$ayhdrufe>");
+  
+  
+}
+function set_title($ethfe){
+  compile("<title>$ethfe</title>");
+  
+  
+  
+}
+function html($hkjgy){
+  compile("<html>$hkjgy</html>");
+  
+}
+?>
