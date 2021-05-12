a23986fb6a27cf999da32c9d0718d0ba8a5e6125
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-13 01:02:10 +0430

    Update style.php

--- style.php
+++ style.php
@@ -1,1 +1,16 @@
-
+<?php
+function style_padding($h311f12rtctxdgygyygyggygygygy,$h311q12idugyewhfrhgfyhgcogw){ echo compile("<style>#$h311f12rtctxdgygyygyggygygygy {Padding:$h311q12idugyewhfrhgfyhgcogw}</style>"); }
+function style_padding_top($h128gh,$h127gh){ echo compile("<style>#$h128gh {Padding-top:$h127gh}</style>"); }
+function style_padding_bottom($h1128gh,$h1127gh){ echo compile("<style>#$h1128gh {Padding-bottom:$h1127gh}</style>"); }
+function style_padding_left($h11128gh,$h11127gh){ echo compile("<style>#$h11128gh {Padding-left:$h11127gh}</style>"); }
+function style_padding_right($h111128gh,$h111127gh){ echo compile("<style>#$h111128gh {Padding-right:$h111127gh}</style>"); }
+function style_position($almk,$wfikftu){ echo compile("<style>#$almk {Position:$wfikftu}</style>"); }
+function style_top($a1lmk,$w1fikftu){ echo compile("<style>#$a1lmk {Top:$w1fikftu}</style>"); }
+function style_bottom($a11lmk,$w11fikftu){ echo compile("<style>#$a11lmk {Bottom:$w11fikftu}</style>"); }
+function style_left($a111lmk,$w111fikftu){ echo compile("<style>#$a111lmk {Left:$w111fikftu}</style>"); }
+function style_right($a1111lmk,$w1111fikftu){ echo compile("<style>#$a1111lmk {Right:$w1111fikftu}</style>"); }
+function style_visibility($a1111lmk,$w1111fikftu){ echo compile("<style>#$a11111lmk {Visibility:$w11111fikftu}</style>"); }
+function style_opacity($a211111lmk,$w21111fikftu){ echo compile("<style>#$a211111lmk {Opacity:$w2111111fikftu}</style>"); }
+function style_float($a111111lmk,$w11111fikftu){ echo compile("<style>#$a111111lmk {Float:$w1111111fikftu}</style>"); }
+function style_clear($a1111111lmk,$w111111fikftu){ echo compile("<style>#$a1111111lmk {Clear:$w11111111fikftu}</style>"); }
+?>
