3c9c2ede1bcf87fdc3ea5f297b84555b8164658b
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-19 21:53:08 +0430

    Update Func.php

--- Func.php
+++ Func.php
@@ -136,6 +136,40 @@ var input128 = document.getElementById('$afvdd').value;
   document.getElementById('p1').innerHTML = input128;
 </script>");
 }
+function patch_info($patch_12y,$met_1fv){
+  $path_parts1 = pathinfo($patch_12y);
+ 
+// دایرکتری فایل
+$directoryName1 = $path_parts1['dirname'];
+ 
+// نام اصلی فایل
+$baseFileName1 = $path_parts1['basename'];
+ 
+// پسوند فایل
+$fileExtension1 = $path_parts1['extension'];
+ 
+// نام فایل
+$fileName1 = $path_parts1['filename'];
+//switch
+switch ($met_1fv) {
+  case 'size':
+    return filesize($patch_12y);
+    break;
+    case 'name':
+      return $fileName1;
+      break;
+      case 'type':
+        return $fileExtension1;
+        break;
+        case 'basename':
+         return $baseFileName1;
+          break;
+          case 'dir':
+            return $directoryName1;
+            break;
+}
+}
+
 function download_in_patch($url90, $save_path90) {
      set_time_limit (0);
      $f90 = fopen($save_path90, 'w+');
