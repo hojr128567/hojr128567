b961f018f83c787bdea7a4e09f29b689e2d1ee09
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-25 01:35:06 +0430

    Create lottie.php

---
+++ lottie.php
@@ -0,0 +1,7 @@
+<?php
+function lottie($id_lottie778,$json_lottie433){
+  compile("<lottie-player 
+  src='$json_lottie433' id='$id_lottie778'  background='transparent'  speed='1' loop autoplay >
+</lottie-player>");
+}
+?>
