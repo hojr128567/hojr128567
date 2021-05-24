fd9a4cf34ffd49cb2001faa5ec17d25a5cfb6746
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-05-24 18:16:24 +0430

    Create import.php

---
+++ import.php
@@ -0,0 +1,11 @@
+<?php
+function import($dd0pw){
+  //
+  switch ($dd0pw110) {
+    case 'lottie':
+      compile('<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>');
+      break;
+  }
+  //
+}
+?>
