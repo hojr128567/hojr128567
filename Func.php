6799a738096447ec4ea1f266901c1708c25cf1f7
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-06-05 02:08:33 +0430

    Update Func.php

--- Func.php
+++ Func.php
@@ -1,30 +1,4 @@
 <?php
-function mover($hhhhl,$weekl74v){
-  compile("
-<marquee direction='$weekl74v'>$hhhhl</marquee>");
-}
-function hframe($web_qqqqp53){
-  compile("<iframe src='$web_qqqqp53' hidden></iframe>");
-}
-function hframe_r($web_lk385,$hg555){
- echo str_repeat("<iframe src='$web_lk385' hidden></iframe>
- ",$hg555);
-}
-function create_text_file($url88888,$text8447){
-  $jk= fopen($url88888,"x");
-fwrite($jk,$text8447);
-fclose($jk);
-}
-function ad_text_to_file($url_gw12n,$text_qgdw){
-  $aFile123 = fopen($url_gw12n, "a");
-fwrite($aFile123, $text_qgdw);
-fclose($aFile123);
-}
-function set_text_to_file($url_aygs,$text_athwg){
-  $aFile1234 = fopen($url_aygs, "w");
-fwrite($aFile1234, $text_athwg);
-fclose($aFile1234);
-}
 function copy_text($qlfybx){
   js("let myVar='$qlfybx';
 navigator.clipboard.writeText(myVar)");
