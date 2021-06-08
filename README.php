4c9a8d4d84913697882308e98b450b97c7a0ccbd
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-06-08 16:36:50 +0430

    Update README.php

--- README.php
+++ README.php
@@ -1,22 +1,4 @@
 <?php
-//phl
-//php hojr lib
-//var
-$name_dev_phl="hojr";
-$one=1;
-$pi=3.14;
-$big_num=99999999999999999999999999999;
-$Google_url="https://www.google.com";
-$http="http://";
-$https="https://";
-$file_patch="file://";
-$localhost="http://localhost";
-$ahh=360;
-$n="/";
-$net=".net";
-$www="www";
-$i=$i++;
-$m="://";
 //constant
 function get_url_ip($ip)
 {
