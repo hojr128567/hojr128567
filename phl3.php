439628335364fcb04d0d6f6f0fae2fe270adf60d
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-06-08 15:55:41 +0430

    Update phl3.php

--- phl3.php
+++ phl3.php
@@ -1,1 +1,36 @@
-
+<?php
+//style
+function style_width($jqueuhdhdhdhdh,$hqidugyecogw){
+  echo compile("<style>#$jqueuhdhdhdhdh {width:$hqidugyecogw}</style>");
+}
+function style_height($hfrtctxdgygyygyggygygygy,$hqidugyewhfrhgfyhgcogw){
+  echo compile("<style>#$hfrtctxdgygyygyggygygygy {height:$hqidugyewhfrhgfyhgcogw}</style>");
+}
+function style_max_width($hf12rtctxdgygyygyggygygygy,$hq12idugyewhfrhgfyhgcogw){
+  echo compile("<style>#$hf12rtctxdgygyygyggygygygy {Max-width:$hq12idugyewhfrhgfyhgcogw}</style>");
+}
+function style_max_height($h1f12rtctxdgygyygyggygygygy,$h1q12idugyewhfrhgfyhgcogw){
+  echo compile("<style>#$h1f12rtctxdgygyygyggygygygy {Max-height:$h1q12idugyewhfrhgfyhgcogw}</style>");
+}
+function style_min_width($h21f12rtctxdgygyygyggygygygy,$h21q12idugyewhfrhgfyhgcogw){
+  echo compile("<style>#$h21f12rtctxdgygyygyggygygygy {Min-width:$h21q12idugyewhfrhgfyhgcogw}</style>");
+}
+function style_min_height($h321f12rtctxdgygyygyggygygygy,$h321q12idugyewhfrhgfyhgcogw){
+  echo compile("<style>#$h321f12rtctxdgygyygyggygygygy {Min-height:$h321q12idugyewhfrhgfyhgcogw}</style>");
+}
+function style_margin($qjswivs,$qubsu){
+  echo compile("<style>#$qjswivs {Margin:$qubsu}</style>");
+}
+function style_margin_top($q1jswivs,$q2ubsu){
+  echo compile("<style>#$q1jswivs {Margin-top:$q2ubsu}</style>");
+}
+function style_margin_bottom($q12jswivs,$q22ubsu){
+  echo compile("<style>#$q12jswivs {Margin-bottom:$q22ubsu}</style>");
+}
+function style_margin_left($q112jswivs,$q122ubsu){
+  echo compile("<style>#$q112jswivs {Margin-left:$q122ubsu}</style>");
+}
+function style_margin_right($q2112jswivs,$q2122ubsu){
+  echo compile("<style>#$q2112jswivs {Margin-right:$q2122ubsu}</style>");
+}
+?>
