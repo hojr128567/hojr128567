610c2582e6642113d64fb274bb9f41f6dba634d4
Phl official <82045061+hojr128567@users.noreply.github.com>
2021-06-12 17:54:57 +0430

    Delete lottie.php

--- lottie.php
+++
@@ -1,7 +0,0 @@
-<?php
-function lottie($id_lottie778,$json_lottie433){
-  compile("<lottie-player 
-  src='$json_lottie433' id='$id_lottie778'  background='transparent'  speed='1' loop autoplay >
-</lottie-player>");
-}
-?>
